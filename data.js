8d0d96b814e8be69c3891e3d4210cfdc64070599
Erik <erikschmidt3001@gmail.com>
2026-07-11 12:17:31 +0300

    Extend scenes in data.js to include all images from images/

--- data.js
+++ data.js
@@ -1,87 +1,71 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "ground-riihintupa-northeast",
-      "name": "Riihintupa - Ground - Northeast",
+      "id": "120m-riihintupa-bay",
+      "name": "120m riihintupa bay",
       "levels": [
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
       ],
       "faceSize": 1500,
-      "initialViewParameters": {
-        "yaw": -10 * Math.PI/180,
-        "pitch": 1 * Math.PI/180,
-        "fov": 1.5708
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0, 
-          "pitch": -90 * Math.PI / 180,
-          "rotation": 0,
-          "target": "30m-riihintupa-northeast"
-        },
-        {
-          "yaw": -50 * Math.PI / 180, 
-          "pitch": -9 * Math.PI / 180,
-          "rotation": 0,
-          "target": "ground-riihintupa-southeast"
-        }
-      ],
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "ground-riihintupa-southeast",
-      "name": "Riihintupa - Ground - Southeast",
+      "id": "30m-island",
+      "name": "30m island",
       "levels": [
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
       ],
       "faceSize": 1500,
-      "initialViewParameters": {
-        "yaw": 0.0,
-        "pitch": 0.1,
-        "fov": 1.5708
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.0, 
-          "pitch": -0.5,
-          "rotation": 0,
-          "target": "ground-riihintupa-northeast"
-        },
-        {
-          "yaw": 0.6, 
-          "pitch": -1.1,
-          "rotation": 0,
-          "target": "30m-riihintupa-northeast"
-        }
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "30m-riihintupa-bay",
+      "name": "30m riihintupa bay",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
       ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "30m-riihintupa-beach",
+      "name": "30m riihintupa beach",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "30m-riihintupa-northeast-2",
+      "name": "30m riihintupa northeast 2",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
       "id": "30m-riihintupa-northeast",
       "name": "Riihintupa - 30 meters",
       "levels": [
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
       ],
       "faceSize": 1500,
       "initialViewParameters": {
@@ -89,14 +73,159 @@ var APP_DATA = {
         "pitch": 65 * Math.PI / 180,
         "fov": 1.5708
       },
-      "linkHotspots": [
-        {
-          "yaw": -0.3, 
-          "pitch": 1.2,
-          "rotation": 0,
-          "target": "ground-riihintupa-northeast"
-        }
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "80m-bay",
+      "name": "80m bay",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
       ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "80m-island",
+      "name": "80m island",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "80m-riihintupa-bay",
+      "name": "80m riihintupa bay",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "80m-riihintupa-northeast",
+      "name": "80m riihintupa northeast",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-island",
+      "name": "ground island",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-bay",
+      "name": "ground riihintupa bay",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-beach",
+      "name": "ground riihintupa beach",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-lake",
+      "name": "ground riihintupa lake",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-northeast",
+      "name": "Riihintupa - Ground - Northeast",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -10 * Math.PI/180,
+        "pitch": 1 * Math.PI/180,
+        "fov": 1.5708
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-northwest",
+      "name": "ground riihintupa northwest",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-roof",
+      "name": "ground riihintupa roof",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": { "yaw": 0.0, "pitch": 0.0, "fov": 1.5708 },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "ground-riihintupa-southeast",
+      "name": "Riihintupa - Ground - Southeast",
+      "levels": [
+        { "tileSize": 512, "size": 1024 },
+        { "tileSize": 512, "size": 2048 }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.0,
+        "pitch": 0.1,
+        "fov": 1.5708
+      },
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
